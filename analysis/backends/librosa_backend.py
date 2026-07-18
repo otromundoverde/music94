8797ee4f8ccd9c5f1fdfdeac53657417246b1b05
Francisco Javier Cisternas <franciscocisternas94@gmail.com>
2026-07-18 00:20:40 -0400

    v0.5.3 First audio loading

--- analysis/backends/librosa_backend.py
+++ analysis/backends/librosa_backend.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from analysis.backends.audio_backend import AudioBackend
 from analysis.features.feature_set import FeatureSet
 
@@ -35,8 +37,44 @@ class LibrosaBackend(AudioBackend):
 
             return features
 
-        #
-        # El análisis real comenzará en v0.5.3
-        #
+        try:
+
+            audio_path = Path(song.path)
+
+            if not audio_path.exists():
+
+                return features
+
+            y, sr = self.librosa.load(
+
+                audio_path,
+
+                sr=None,
+
+                mono=True,
+
+            )
+
+            print(
+
+                f"[Music94] Loaded: {audio_path.name}"
+
+            )
+
+            print(
+
+                f"Samples: {len(y):,}"
+
+            )
+
+            print(
+
+                f"Sample Rate: {sr}"
+
+            )
+
+        except Exception as error:
+
+            print(error)
 
         return features
