1da97eaf6a4088517c91c507b30d9e1303f02f93
Francisco Javier Cisternas <franciscocisternas94@gmail.com>
2026-07-17 23:51:44 -0400

    v0.3.1 Dynamic Query Builder

--- database/query_engine.py
+++ database/query_engine.py
@@ -13,16 +13,16 @@ class QueryEngine:
 
         cursor = self.db.cursor()
 
-        sql = f"""
+        sql = """
             SELECT *
             FROM songs
         """
 
         if where:
 
-            sql += f"\nWHERE {where}"
+            sql += "\nWHERE " + where
 
-        sql += f"\nORDER BY {order}"
+        sql += "\nORDER BY " + order
 
         cursor.execute(sql, params)
 
@@ -30,102 +30,87 @@ class QueryEngine:
 
     # ---------------------------------------------------------
 
+    def search(
+
+        self,
+
+        artist=None,
+        album=None,
+        genre=None,
+        year_min=None,
+        year_max=None,
+        bpm_min=None,
+        bpm_max=None,
+        musical_key=None,
+        label=None,
+        country=None,
+
+    ):
+
+        where = []
+
+        params = []
+
+        if artist:
+
+            where.append("artist = ?")
+            params.append(artist)
+
+        if album:
+
+            where.append("album = ?")
+            params.append(album)
+
+        if genre:
+
+            where.append("genre = ?")
+            params.append(genre)
+
+        if year_min is not None:
+
+            where.append("CAST(year AS INTEGER) >= ?")
+            params.append(year_min)
+
+        if year_max is not None:
+
+            where.append("CAST(year AS INTEGER) <= ?")
+            params.append(year_max)
+
+        if bpm_min is not None:
+
+            where.append("bpm >= ?")
+            params.append(bpm_min)
+
+        if bpm_max is not None:
+
+            where.append("bpm <= ?")
+            params.append(bpm_max)
+
+        if musical_key:
+
+            where.append("musical_key = ?")
+            params.append(musical_key)
+
+        if label:
+
+            where.append("label = ?")
+            params.append(label)
+
+        if country:
+
+            where.append("country = ?")
+            params.append(country)
+
+        return self.execute(
+
+            " AND ".join(where),
+
+            tuple(params)
+
+        )
+
+    # ---------------------------------------------------------
+
     def all(self):
 
         return self.execute()
-
-    # ---------------------------------------------------------
-
-    def artist(self, artist):
-
-        return self.execute(
-
-            "artist = ?",
-
-            (artist,)
-
-        )
-
-    # ---------------------------------------------------------
-
-    def album(self, album):
-
-        return self.execute(
-
-            "album = ?",
-
-            (album,)
-
-        )
-
-    # ---------------------------------------------------------
-
-    def genre(self, genre):
-
-        return self.execute(
-
-            "genre = ?",
-
-            (genre,)
-
-        )
-
-    # ---------------------------------------------------------
-
-    def year_between(self, start, end):
-
-        return self.execute(
-
-            "CAST(year AS INTEGER) BETWEEN ? AND ?",
-
-            (start, end)
-
-        )
-
-    # ---------------------------------------------------------
-
-    def bpm_between(self, minimum, maximum):
-
-        return self.execute(
-
-            "bpm BETWEEN ? AND ?",
-
-            (minimum, maximum)
-
-        )
-
-    # ---------------------------------------------------------
-
-    def key(self, musical_key):
-
-        return self.execute(
-
-            "musical_key = ?",
-
-            (musical_key,)
-
-        )
-
-    # ---------------------------------------------------------
-
-    def label(self, label):
-
-        return self.execute(
-
-            "label = ?",
-
-            (label,)
-
-        )
-
-    # ---------------------------------------------------------
-
-    def country(self, country):
-
-        return self.execute(
-
-            "country = ?",
-
-            (country,)
-
-        )
